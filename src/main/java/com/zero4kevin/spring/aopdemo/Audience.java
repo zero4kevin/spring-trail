0ef5a7d334f9d4fe45c9e6a22d5a082a9cef376c
xi1zhang <kevin.zhang@nokia-sbell.com>
2017-11-03 18:03:25 +0800

    add aop examples

---
+++ src/main/java/com/zero4kevin/spring/aopdemo/Audience.java
@@ -0,0 +1,22 @@
+package com.zero4kevin.spring.aopdemo;
+
+/**
+ * Created by xi1zhang on 2017/11/3.
+ */
+public class Audience {
+    public void takeSeats() {
+        System.out.println("The audience is taking their seats.");
+    }
+
+    public void turnOffCellPhones() {
+        System.out.println("The audience is turning off their cellphones");
+    }
+
+    public void applaud(){
+        System.out.println("CLAP CLAP CLAP");
+    }
+
+    public void demandRefund() {
+        System.out.println("Boo! We want our money back!");
+    }
+}
