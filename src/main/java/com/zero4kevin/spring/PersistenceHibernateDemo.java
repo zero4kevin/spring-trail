c698009c5093fee2824e6e2c027f510a3e8f19df
xi1zhang <kevin.zhang@nokia-sbell.com>
2017-12-26 13:26:56 +0800

    a little update

--- src/main/java/com/zero4kevin/spring/PersistenceHibernateDemo.java
+++ src/main/java/com/zero4kevin/spring/PersistenceHibernateDemo.java
@@ -6,7 +6,6 @@ import org.hibernate.SessionFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
-
 /**
  * Created by xi1zhang on 2017/11/27.
  */
@@ -14,10 +13,10 @@ public class PersistenceHibernateDemo {
     public static void main(String[] args){
         ApplicationContext context=new FileSystemXmlApplicationContext("src/main/java/com/zero4kevin/spring/spring-hibernate.xml");
         PersistenceOperationsDAO dao=(PersistenceOperationsDAO) context.getBean("hibernateDao");
-        Iterator iterator=dao.queryAll().iterator();
-        while(iterator.hasNext()) {
-            System.out.println(iterator.next());
-        }
+//        Iterator iterator=dao.queryAll().iterator();
+//        while(iterator.hasNext()) {
+//            System.out.println(iterator.next());
+//        }
 
     }
 }
